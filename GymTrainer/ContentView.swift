1d20848f74915eb2295ab287f749e2d9a5c3ccbe
Tsolis Dimitris Sotiris <dimitristls@gmail.com>
2019-10-12 12:17:43 +0300

    Initial Commit

---
+++ GymTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymTrainer
+//
+//  Created by Dimitris-Sotiris Tsolis on 12/10/2019.
+//  Copyright © 2019 DFG Team. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymTrainerTests/GymTrainerTests.swift
@@ -0,0 +1,34 @@
+//
+//  GymTrainerTests.swift
+//  GymTrainerTests
+//
+//  Created by Dimitris-Sotiris Tsolis on 12/10/2019.
+//  Copyright © 2019 DFG Team. All rights reserved.
+//
+
+import XCTest
+@testable import GymTrainer
+
+class GymTrainerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
